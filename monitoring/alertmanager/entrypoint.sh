cd86dd83dc174ccf7b2c81c45d899d2b4875e15a
AutomationArtist01 <rohithgurrala14@gmail.com>
2026-08-18 11:21:19 +0530

    feat(alertmanager): severity/category routing to Slack + e-mail, inhibition, Slack template, env entrypoint

---
+++ monitoring/alertmanager/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Render env vars into the config (Alertmanager has no native env substitution), then start.
+set -e
+: "${SMTP_HOST:=mailhog}"; : "${SMTP_PORT:=1025}"; : "${SMTP_FROM:=alerts@team4.local}"
+: "${ALERT_EMAIL_TO:=oncall@team4.local}"; : "${SLACK_WEBHOOK_URL:=http://webhook-catcher:8091/slack}"
+export SMTP_HOST SMTP_PORT SMTP_FROM ALERT_EMAIL_TO SLACK_WEBHOOK_URL
+sed -e "s|\${SMTP_HOST}|$SMTP_HOST|g" -e "s|\${SMTP_PORT}|$SMTP_PORT|g" -e "s|\${SMTP_FROM}|$SMTP_FROM|g" \
+    -e "s|\${ALERT_EMAIL_TO}|$ALERT_EMAIL_TO|g" -e "s|\${SLACK_WEBHOOK_URL}|$SLACK_WEBHOOK_URL|g" \
+    /etc/alertmanager/alertmanager.yml > /tmp/alertmanager.yml
+exec /bin/alertmanager --config.file=/tmp/alertmanager.yml --storage.path=/alertmanager "$@"
